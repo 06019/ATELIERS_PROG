40872903b6d8c56d716d35e22c57861a5032eaa9
ferhat <ferhat.mehdioui@monboreal.ca>
2026-03-07 21:45:09 -0500

    creation des objet de voitures

--- atl3.py
+++ atl3.py
@@ -49,6 +49,11 @@ class Parc :
             print(f"le nombre de places libre est : {x}")
 
 p1=Parc (06,"Bejaia,Algerie",3)
+v1=Voiture("MOB06","TOYOTA","GRISE")
+v2=Voiture("QBC26","HONDA","GRISE")
+v3=Voiture("JSK06","FORD","NOIRE")
+v4=Voiture("JDKF259","NISSAN","BLANCHE")
+
 
 
 
