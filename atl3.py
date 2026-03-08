599e31548c911da0f6675ba48cb8b4e617dbaaaf
ferhat <ferhat.mehdioui@monboreal.ca>
2026-03-07 21:27:55 -0500

    creation de entrervoiture

--- atl3.py
+++ atl3.py
@@ -20,3 +20,17 @@ class Parc :
         self.adresse= adr
         self.capacite = cap
         self.liste_voitures = []
+    def entrer_voiture(self,voiture):
+        for v in self.liste_voitures:
+            if v.matricule == voiture.matricule:
+                print ("votre voiture existe deja ")
+                return
+        if len(self.liste_voitures)>= self.capacite:
+            print ("desole le parc est plien y a plus d'espace")
+            return
+        self.liste_voitures.append(voiture)
+        voiture.voiture_stocke()
+        print ("votre voiture stocke")
+
+
+
