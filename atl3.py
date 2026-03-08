5322aa91fbfb8c7d6bcf530b1e15b959ac623840
ferhat <ferhat.mehdioui@monboreal.ca>
2026-03-07 21:06:53 -0500

    creation de class voiture atributs

---
+++ atl3.py
@@ -0,0 +1,7 @@
+class Voiture :
+    def __init__(self, matricule,marque,coul):
+        self.matricule = matricule
+        self.marque = marque
+        self.couleur = coul
+        self.etat = False
+
