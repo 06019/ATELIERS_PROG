b2aca796b19c0fb7e22b279608ea07c669c39d70
ferhat <ferhat.mehdioui@monboreal.ca>
2026-03-07 21:13:08 -0500

    les methodes de class voiture sont cree

--- atl3.py
+++ atl3.py
@@ -4,4 +4,13 @@ class Voiture :
         self.marque = marque
         self.couleur = coul
         self.etat = False
+    def afficher_infos(self):
+        print(f"la voiture : {self.matricule} , de marque : {self.marque} , de couleur : {self.couleur}")
+
+    def voiture_stocke(self):
+        self.etat = True
+        print(f"la voiture : {self.matricule} est stocke")
+    def voiture_sortie(self):
+        self.etat = False
+        print(f"la voiture : {self.matricule} est sortie")
 
